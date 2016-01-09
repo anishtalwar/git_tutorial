474121cc1590e4a9a446826482ac5958739168f0
Anish Talwar <anishtalwar@Rajivs-MBP.wireless.concordia.ca>
2016-01-09 13:42:19 -0500

    filename is verbose and obvious

--- count.sh
+++ count.sh
@@ -1,9 +1,10 @@
 # Display the name of each data file and count
 # the number of distinct occurrences of each species
 # in that file
-for filename in data/*.txt
-date
+for f in data/*.txt
+
 do 
+    date
     echo $filename
     grep -v Species $filename | cut -d, -f2 | sort | uniq -c | sort -n -r
 done
